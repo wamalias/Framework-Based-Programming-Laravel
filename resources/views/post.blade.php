295d7fa015e9354344909d90e98c2c7e871c245a
wamalias <wardamalias@gmail.com>
2024-09-18 19:47:32 +0700

    Update View Data

---
+++ resources/views/post.blade.php
@@ -0,0 +1,12 @@
+<x-layout>
+    <x-slot:title>{{ $title }}</x-slot:title>
+
+    <article class="py-8 max-w-screen-md border-b border-gray-300">
+        <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900">{{ $post['title'] }}</h2>
+        <div>
+            <a href="#">{{ $post['author'] }}</a> | 15 September 2024
+        </div>
+        <p class="my-4 font-light">{{ $post['body'] }}</p>
+        <a href="/posts" class="font-medium text-blue-500 hover:underline">&laquo; Back to post</a>
+    </article>    
+</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -10,8 +11,48 @@ Route::get('/about', function () {
     return view('about', ['title' => 'About Page', 'nama' => 'Wardatul Amalia Safitri']);
 });
 
-Route::get('/blog', function () {
-    return view('blog', ['title' => 'Blog Page']);
+Route::get('/posts', function () {
+    return view('posts', ['title' => 'Blog Page', 'posts' => [
+        [
+            'id' => 1,
+            'slug' => 'judul-artikel-1',
+            'title' => 'Judul artikel 1',
+            'author' => 'Wardatul Amalia Safitri',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis maxime, minus fugit odit quam error nam necessitatibus saepe? Eum voluptas saepe quos quibusdam obcaecati alias nisi natus reiciendis, nesciunt veniam.'
+        ],
+        [
+            'id' => 2,
+            'slug' => 'judul-artikel-2',
+            'title' => 'Judul artikel 2',
+            'author' => 'Wardatul Amalia Safitri',
+            'body' => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Natus, a atque corporis obcaecati neque ex commodi laboriosam enim veniam adipisci assumenda optio velit accusamus reprehenderit quisquam, cupiditate, impedit explicabo ad.'
+        ]
+    ]]);
+});
+
+Route::get('/posts/{slug}', function($slug) {
+    $posts = [
+        [
+            'id' => 1,
+            'slug' => 'judul-artikel-1',
+            'title' => 'Judul artikel 1',
+            'author' => 'Wardatul Amalia Safitri',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis maxime, minus fugit odit quam error nam necessitatibus saepe? Eum voluptas saepe quos quibusdam obcaecati alias nisi natus reiciendis, nesciunt veniam.'
+        ],
+        [
+            'id' => 2,
+            'slug' => 'judul-artikel-2',
+            'title' => 'Judul artikel 2',
+            'author' => 'Wardatul Amalia Safitri',
+            'body' => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Natus, a atque corporis obcaecati neque ex commodi laboriosam enim veniam adipisci assumenda optio velit accusamus reprehenderit quisquam, cupiditate, impedit explicabo ad.'
+        ]
+    ];
+
+    $post = Arr::first($posts, function($post) use ($slug){
+        return $post['slug'] == $slug;
+    });
+
+    return view('post', ['title' => 'Single Post', 'post' => $post]);
 });
 
 Route::get('/contact', function () {
