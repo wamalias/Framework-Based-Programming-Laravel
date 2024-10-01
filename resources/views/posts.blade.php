e65afbd4620e68441647aac9185af33c3acefb73
wamalias <wardamalias@gmail.com>
2024-10-01 11:05:35 +0700

    Update Eloquent Relationship

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -6,8 +6,8 @@
             <a href="/posts/{{ $post['slug'] }}" class="hover:underline">
                 <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900">{{ $post['title'] }}</h2>
             </a>
-            <div>
-                <a href="#">{{ $post['author'] }}</a> | {{ $post->created_at->format('j F Y') }}
+            <div class="text-base text-gray-500">
+                <a href="/authors/{{ $post->author->id }}" class="hover:underline">{{ $post->author->name }}</a> | {{ $post->created_at->format('j F Y') }}
             </div>
             <p class="my-4 font-light">{{ Str::limit($post['body'], 100) }}</p>
             <a href="/posts/{{ $post['slug'] }}" class="font-medium text-blue-500 hover:underline">Read more &raquo;</a>
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 use App\Models\Post;
+use App\Models\User;
 
 Route::get('/', function () {
     return view('home', ['title' => 'Home Page']);
@@ -17,10 +18,13 @@ Route::get('/posts', function () {
 });
 
 Route::get('/posts/{post:slug}', function(Post $post) {
-
     return view('post', ['title' => 'Single Post', 'post' => $post]);
 });
 
 Route::get('/contact', function () {
     return view('contact', ['title' => 'Contact Page']);
+});
+
+Route::get('/authors/{user}', function(User $user) {
+    return view('posts', ['title' => 'Articles by ' . $user->name, 'posts' => $user->posts]);
 });
