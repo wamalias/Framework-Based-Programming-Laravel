f55535d97e4f55be055a7805f55072b036df4235
wamalias <wardamalias@gmail.com>
2024-10-08 09:41:44 +0700

    Update Searching

--- routes/web.php
+++ routes/web.php
@@ -15,9 +15,7 @@ Route::get('/about', function () {
 });
 
 Route::get('/posts', function () {
-    // $posts = Post::with(['author', 'category'])->latest()->get();
-    $posts = Post::latest()->get();
-    return view('posts', ['title' => 'Blog Page', 'posts' => $posts]);
+    return view('posts', ['title' => 'Blog Page', 'posts' => Post::filter(request(['search', 'category', 'author']))->latest()->get()]);
 });
 
 Route::get('/posts/{post:slug}', function(Post $post) {
